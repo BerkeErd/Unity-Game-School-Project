cbfbc048a8cc7f14be48601d7469a3adfbb23520
yahyacan98 <sahanyahya@hotmail.com>
2021-10-03 21:21:51 +0300

    Güncelleme
    +fighter run animasyonu yapıldı
    +hareket scripti

---
+++ BeatEmUp/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int runSpeed = 1;
+
+    float horizontal;
+    float vertical;
+    Animator animator;
+    bool facingRight;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal != 0 ? horizontal : vertical));
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(horizontal * runSpeed, vertical * runSpeed, 0.0f);
+        transform.position = transform.position + movement * Time.deltaTime;
+        Flip(horizontal);
+
+    }
+
+    private void Flip(float horizontal)
+    {
+        if(horizontal < 0 && !facingRight || horizontal > 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+}
