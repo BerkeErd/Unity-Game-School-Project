5fdbccda47a11f3bed63df5037a6af90c59b5a01
Berke Erdeniz <39529687+BerkeErd@users.noreply.github.com>
2021-10-03 23:01:20 +0300

    Güncelleme
    +Joystick eklendi
    +Koşma animasyonu ayarlandı
    +Fighter'in hareket sınırları eklendi (Y ekseni)

---
+++ BeatEmUp/Assets/Scripts/Joystick.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    public Transform player;
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
+
+    public Transform circle;
+    public Transform outerCircle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Input.mousePosition.x <= Screen.width / 2)
+        {
+            touchStart = true;
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+
+            circle.transform.position = pointA;
+            outerCircle.transform.position = pointA;
+            circle.GetComponent<SpriteRenderer>().enabled = true;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if (Input.GetMouseButton(0) && Input.mousePosition.x <= Screen.width / 2)
+        {
+            
+                
+                pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+                  
+        }
+        else
+        {
+            touchStart = false;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        if (touchStart && Input.mousePosition.x <= Screen.width / 2)
+        {
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+
+            circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y);
+
+            //Player'a yön bilgilerini gönder
+            player.GetComponent<PlayerMovement>().horizontal = direction.x;
+            player.GetComponent<PlayerMovement>().vertical = direction.y;
+        }
+        else
+        {
+            circle.GetComponent<SpriteRenderer>().enabled = false;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+
+            //Player'a yön bilgilerini gönder
+            player.GetComponent<PlayerMovement>().horizontal = 0;
+            player.GetComponent<PlayerMovement>().vertical = 0;
+        }
+
+    }
+
+
+   
+}
--- BeatEmUp/Assets/Scripts/PlayerMovement.cs
+++ BeatEmUp/Assets/Scripts/PlayerMovement.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int runSpeed;
 
-    public int runSpeed = 1;
-
-    float horizontal;
-    float vertical;
+    public float horizontal;
+    public float vertical;
     Animator animator;
     bool facingRight;
+
+    public float maxYpos = -2.21f;
+    public float minYpos = -4.90f;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
         animator.SetFloat("Speed", Mathf.Abs(horizontal != 0 ? horizontal : vertical));
     }
 
@@ -31,6 +30,21 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 movement = new Vector3(horizontal * runSpeed, vertical * runSpeed, 0.0f);
         transform.position = transform.position + movement * Time.deltaTime;
+
+        if (transform.position.y >= maxYpos)
+        {
+            transform.position = new Vector2(transform.position.x, maxYpos - 0.01f);
+        }
+
+        else if (transform.position.y <= minYpos)
+        {
+            transform.position = new Vector2(transform.position.x, minYpos-0.01f);
+        }
+
+
+
+
+
         Flip(horizontal);
 
     }
