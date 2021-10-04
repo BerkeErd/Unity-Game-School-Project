f80d415c1f176cd2066413c0e9369842546e7bd1
Berke Erdeniz <39529687+BerkeErd@users.noreply.github.com>
2021-10-04 14:04:44 +0300

    yumruk animasyonu pointer sorunlu hali

--- BeatEmUp/Assets/Scripts/PlayerMovement.cs
+++ BeatEmUp/Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public int runSpeed;
 
@@ -11,25 +13,71 @@ public class PlayerMovement : MonoBehaviour
     Animator animator;
     bool facingRight;
 
+    bool isPunching;
+
+    Button punchButton;
     public float maxYpos = -2.21f;
     public float minYpos = -4.90f;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-      
+        punchButton = GameObject.Find("PunchButton").GetComponent<Button>();
+
+        punchButton.onClick.AddListener(delegate { Punch(); });
     }
 
     // Update is called once per frame
     void Update()
     {
         animator.SetFloat("Speed", Mathf.Abs(horizontal != 0 ? horizontal : vertical));
+
+        
+
+    }
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " Was Clicked.");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " Was Released.");
+    }
+
+
+
+
+
+
+
+
+
+    void Punch()
+    {
+        isPunching = true;
+        if (vertical != 0 || horizontal != 0)
+        {
+            vertical = 0;
+            horizontal = 0;
+            animator.SetFloat("Speed", 0);         
+        }
+
+        animator.SetTrigger("Punch");
+
     }
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(horizontal * runSpeed, vertical * runSpeed, 0.0f);
-        transform.position = transform.position + movement * Time.deltaTime;
+        if((horizontal != 0 || vertical != 0) && !isPunching)
+        {
+            Vector3 movement = new Vector3(horizontal * runSpeed, vertical * runSpeed, 0.0f);
+            transform.position = transform.position + movement * Time.deltaTime;
+        }
+       
 
         if (transform.position.y >= maxYpos)
         {
@@ -47,6 +95,14 @@ public class PlayerMovement : MonoBehaviour
 
         Flip(horizontal);
 
+    }
+
+    public void AlertObservers(string message)
+    {
+        if(message == "PunchEnded")
+        {
+            isPunching = false;
+        }
     }
 
     private void Flip(float horizontal)
