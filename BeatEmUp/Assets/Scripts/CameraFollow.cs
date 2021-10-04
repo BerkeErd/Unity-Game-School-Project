72991a290174c6c5b717ab1808c25d7981543b92
Berke Erdeniz <39529687+BerkeErd@users.noreply.github.com>
2021-10-04 15:26:48 +0300

    Takip eden kamera eklendi

---
+++ BeatEmUp/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    private void Start()
+    {
+        player = GameObject.Find("Fighter").GetComponent<Transform>();
+    }
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+    }
+
+}
