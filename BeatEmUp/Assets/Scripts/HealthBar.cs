d20d7247b49ade07295ab521524dfd29e6da76f3
yahyacan98 <sahanyahya@hotmail.com>
2021-10-08 14:19:58 +0300

    .
    .

--- BeatEmUp/Assets/Scripts/HealthBar.cs
+++ BeatEmUp/Assets/Scripts/HealthBar.cs
@@ -16,14 +16,14 @@ public class HealthBar : MonoBehaviour
 		slider.maxValue = health;
 		slider.value = health;
 
-		//fill.color = gradient.Evaluate(1f);
+		fill.color = gradient.Evaluate(1f);
 	}
 
 	public void SetHealth(int health)
 	{
 		slider.value = health;
 
-	    //fill.color = gradient.Evaluate(slider.normalizedValue);
+	    fill.color = gradient.Evaluate(slider.normalizedValue);
 	}
 
 }
