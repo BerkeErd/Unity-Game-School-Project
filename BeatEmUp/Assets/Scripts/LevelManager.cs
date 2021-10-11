6f44270ecbf69923f8635a0051e7686381c0a3dd
Berke Erdeniz <39529687+BerkeErd@users.noreply.github.com>
2021-10-11 16:16:54 +0300

    Güncelleme
    Sprite atlas ın adı değiştirildi ve sprite doyasına taşındı ayrıca kullanıaln bütün spritelar içine atıldı. Menüye test ekranına girmek için buton eklendi.

--- BeatEmUp/Assets/Scripts/LevelManager.cs
+++ BeatEmUp/Assets/Scripts/LevelManager.cs
@@ -10,4 +10,9 @@ public class LevelManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void LoadTestMap()
+    {
+        SceneManager.LoadScene("TileMapTest");
+    }
 }
