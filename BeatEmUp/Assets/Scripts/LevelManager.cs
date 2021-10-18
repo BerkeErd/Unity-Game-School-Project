67fbcc5da966f406c7719de6ac6e04c0c4f8066e
Berke Erdeniz <39529687+BerkeErd@users.noreply.github.com>
2021-10-18 15:49:48 +0300

    LevelName Text eklendi

--- BeatEmUp/Assets/Scripts/LevelManager.cs
+++ BeatEmUp/Assets/Scripts/LevelManager.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-  
+    public string LevelName;
+    public Text LevelNameText;
+
+    private void Start()
+    {
+        LevelName = SceneManager.GetActiveScene().name;
+        if(LevelNameText)
+        LevelNameText.text = LevelName;
+    }
     public void LoadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
