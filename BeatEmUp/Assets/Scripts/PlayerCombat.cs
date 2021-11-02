b12c8fca67cad67444ad011abaac16519da253f7
Berke Erdeniz <39529687+BerkeErd@users.noreply.github.com>
2021-11-02 16:10:10 +0300

    Revert "Güncelleme"
    This reverts commit 4a5e241bc9373486464dff36d4fc9736ddc26ad4, reversing
    changes made to 92fabf8d598f9259274ba0bc92f492b68aabbc96.

--- BeatEmUp/Assets/Scripts/PlayerCombat.cs
+++ BeatEmUp/Assets/Scripts/PlayerCombat.cs
@@ -54,7 +54,6 @@ public class PlayerCombat : MonoBehaviour
     public Text LevelText;
     public LevelManager levelManager;
     public SaveData saveData;
-    public GameObject RestartMenu;
 
     // Start is called before the first frame update
     private void Awake()
@@ -81,7 +80,6 @@ public class PlayerCombat : MonoBehaviour
         punchDamage = skills.punchDamage;
         kickDamage = skills.kickDamage;
         soundmanager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
-        RestartMenu = GameObject.Find("RestartMenu");
 
         ExpBar.maxValue = skills.PlayerLevel * 20;
 
@@ -99,7 +97,6 @@ public class PlayerCombat : MonoBehaviour
         animator.SetFloat("AttackSpeed", 1 + skills.agiRatio * 5);
 
         LevelText.text = "Level : " + skills.PlayerLevel;
-        RestartMenu.SetActive(false);
 
         UpdatehealthBar();
     }
@@ -348,8 +345,12 @@ public class PlayerCombat : MonoBehaviour
 
     void Die()
     {
+
+
         isDead = true;
         animator.SetBool("IsDead", isDead);
+
+
     }
 
     public void UpdatehealthBar()
@@ -369,12 +370,6 @@ public class PlayerCombat : MonoBehaviour
         if (message == "KickEnded")
         {
             isKicking = false;
-        }
-
-        if (message == "PlayerDie")
-        {
-            RestartMenu.SetActive(true);
-            Time.timeScale = 0;
         }
 
     }
